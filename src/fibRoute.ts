b072c2d0bdb55d2ab5bc0f51a9654b6c233b7498
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2024-01-22 13:29:54 -0500

    Added types to parsed numbers and req and res

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,11 +1,13 @@
 // Endpoint for querying the fibonacci numbers
 
+import { Request, Response } from "express";
 import fibonacci from "./fib";
 
-export default (req: any, res: any) => {
+export default (req: Request, res: Response) => {
   const { num } = req.params;
+  const parsedNum: number = parseInt(num);
 
-  const fibN = fibonacci(parseInt(num));
+  const fibN: number = fibonacci(parsedNum) as number;
   let result = `fibonacci(${num}) is ${fibN}`;
 
   if (fibN < 0) {
